ef404b311bd8280569db4296c04a9e502e00e63a
vvvbbbcz <vvvbbbcz@163.com>
2022-05-02 00:35:47 +0800

    勉强能用

--- page.php
+++ page.php
@@ -1,1 +1,8 @@
-<?php
+<?php get_header(); ?>
+	<div id="content">
+		<main id="main" class="site-main">
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+		</main>
+		<?php get_sidebar(); ?>
+	</div>
+<?php get_footer(); //TODO?>
