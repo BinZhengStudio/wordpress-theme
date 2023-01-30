d7bb138ed734838bbcff9ac90a22ed585fcb0f4e
vvvbbbcz <vvvbbbcz@163.com>
2023-01-30 15:34:18 +0800

    添加icp

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer id="footer">
     <p class="copyright">
-        &copy; <?php bloginfo( 'name' ); ?> | 网站基于 <a target="_blank" href="https://cn.wordpress.org/" rel="noreferrer">WordPress</a>
-        | 主题由 <a target="_blank" href="https://bzgzs.cn/" rel="noreferrer">斌政工作室</a> 开发
+        &copy; <?php bloginfo( 'name' ); ?> | <a href="https://icp.gov.moe/?keyword=20232220" target="_blank">萌ICP备20232220号</a>
+        | 网站基于 <a target="_blank" href="https://cn.wordpress.org/" rel="noreferrer">WordPress</a> | 主题由 <a
+                target="_blank" href="https://bzgzs.cn/" rel="noreferrer">斌政工作室</a> 开发
     </p>
 </footer>
 </div>
